2b23fc813866aab2765f7f528d611d8079ee66a7
Erick Merchant <erickmerchant@gmail.com>
2014-11-05 17:18:51 -0800

    Add files

---
+++ index.js
@@ -0,0 +1,26 @@
+var yaml = require('js-yaml');
+var delim = "---\n";
+var assign = require('object-assign');
+
+module.exports = function () {
+
+    return function (file, page, next) {
+
+        var content = page.content;
+
+        content += "\n";
+
+        content = content.split(delim);
+
+        if(content.length > 1) {
+
+            page = assign(page, yaml.load(content[1]));
+
+            content = content.slice(2);
+        }
+
+        page.content = content.join(delim);
+
+        return next(file, page);
+    };
+};
