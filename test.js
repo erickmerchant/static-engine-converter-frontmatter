fba8995036de760735022473fe0846949fea0847
Erick Merchant <erickmerchant@gmail.com>
2015-10-25 03:07:21 -0700

    Switch tests to tap

--- test.js
+++ test.js
@@ -1,82 +1,78 @@
 var plugin = require('./index.js')
-var assert = require('assert')
-var describe = require('mocha').describe
-var it = require('mocha').it
+var tap = require('tap')
 
-describe('plugin', function () {
-  it('should read frontmatter from the top of the file surrounded by three dashes', function (done) {
-    var frontmatter = plugin(JSON.parse)
+tap.test('should read frontmatter from the top of the file surrounded by three dashes', function (t) {
+  var frontmatter = plugin(JSON.parse)
 
+  frontmatter([{
+    file: 'test.txt',
+    content: '---\n{ "a-property": "value 1", "another-property": "value 2" }\n---\ntest'
+  }], function (err, pages) {
+    t.equal(null, err)
+    t.deepEqual(pages[0], {
+      content: 'test',
+
+      file: 'test.txt',
+
+      'a-property': 'value 1',
+
+      'another-property': 'value 2'
+    })
+
+    t.end()
+  })
+})
+
+tap.test('should read frontmatter from the top of the file surrounded by three dashes', function (t) {
+  var frontmatter = plugin(function () {
+    throw new Error('parse error!')
+  })
+
+  frontmatter([{
+    file: 'test.txt',
+    content: '---\n{ "a-property": "value 1", "another-property": "value 2" }\n---\ntest'
+  }], function (err, pages) {
+    t.equal('parse error!', err.message)
+
+    t.end()
+  })
+})
+
+tap.test('should only read frontmatter', function (t) {
+  var frontmatter = plugin(JSON.parse)
+  var promises = []
+
+  promises.push(new Promise(function (resolve) {
     frontmatter([{
       file: 'test.txt',
-      content: '---\n{ "a-property": "value 1", "another-property": "value 2" }\n---\ntest'
+      content: '---\ntest'
     }], function (err, pages) {
-      assert.equal(null, err)
-      assert.deepEqual(pages[0], {
+      t.equal(null, err)
+      t.deepEqual(pages[0], {
+        content: '---\ntest',
+
+        file: 'test.txt'
+      })
+
+      resolve()
+    })
+  }))
+
+  promises.push(new Promise(function (resolve) {
+    frontmatter([{
+      file: 'test.txt',
+      content: 'test'
+    }], function (err, pages) {
+      t.equal(null, err)
+      t.deepEqual(pages[0], {
         content: 'test',
 
-        file: 'test.txt',
-
-        'a-property': 'value 1',
-
-        'another-property': 'value 2'
+        file: 'test.txt'
       })
 
-      done()
+      resolve()
     })
-  })
+  }))
 
-  it('should read frontmatter from the top of the file surrounded by three dashes', function (done) {
-    var frontmatter = plugin(function () {
-      throw new Error('parse error!')
-    })
-
-    frontmatter([{
-      file: 'test.txt',
-      content: '---\n{ "a-property": "value 1", "another-property": "value 2" }\n---\ntest'
-    }], function (err, pages) {
-      assert.equal('parse error!', err.message)
-
-      done()
-    })
-  })
-
-  it('should only read frontmatter', function (done) {
-    var frontmatter = plugin(JSON.parse)
-    var promises = []
-
-    promises.push(new Promise(function (resolve) {
-      frontmatter([{
-        file: 'test.txt',
-        content: '---\ntest'
-      }], function (err, pages) {
-        assert.equal(null, err)
-        assert.deepEqual(pages[0], {
-          content: '---\ntest',
-
-          file: 'test.txt'
-        })
-
-        resolve()
-      })
-    }))
-
-    promises.push(new Promise(function (resolve) {
-      frontmatter([{
-        file: 'test.txt',
-        content: 'test'
-      }], function (err, pages) {
-        assert.equal(null, err)
-        assert.deepEqual(pages[0], {
-          content: 'test',
-
-          file: 'test.txt'
-        })
-
-        resolve()
-      })
-    }))
-
-    Promise.all(promises).then(function () { done() })
-  })
+  Promise.all(promises).then(function () { t.end() })
 })
